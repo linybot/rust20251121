afc369d46e0cb631abad0ad242a406ae84d27a4c
linybot <89718851+linybot@users.noreply.github.com>
2025-12-02 01:30:31 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://12.12.12.12:21114".to_owned()
+    "http://630123.xyz:21114".to_owned()
 }
 
 #[inline]
